33f9d113c503149f97beb68fd9ce9eb752fc6e43
cshipley98 <claireshipley98@gmail.com>
2023-10-05 13:19:57 -0500

    questions defined and css attached

---
+++ script.js
@@ -0,0 +1,32 @@
+// quiz questions
+var questions =  [
+        //question one
+    {
+        questionText:"Which of the following is not a JavaScript Data Type?",
+        options:["a. Number", "b. Undefine", "c. Boolean", "d. Float",]
+        answer:"d. Float"
+    },
+    // question two
+    questionText:"Inside with HTML element do we put the Javascript?",
+    options:["a. <script>", "b. <head>", "c. <javascript>", "d. <style>"],
+    answer:"a. <script>"
+    {
+//questions three
+        questionText:"JavaScript ignores...",
+        options:["a. Spaces", "b. Tabs", "c. Newlines", "d. All of the above"],
+        answer:"d. All of the above"
+    },
+//question four
+    {
+        questionText:"Which symbol is used for comments in JavaScript?",
+        options:["a. \\", "b. #", "c. //", "d. \* */"],
+        answer:"c. //"
+    },
+// question five
+    {
+        questionText:"Arrays in JavaScript can be used to store____.",
+        options:["a. Numbers and Strings", "b. Other Arrays", "c. Objects", "d. All of the above"],
+        answer:"d. All of the above"
+    }
+
+];
